7375816f38dda918149c8003257b56909ec593dd
Kriván Zoltán <krivan.zoltan@gmail.com>
2020-05-16 10:20:38 +0200

    divison function added

--- team2/index.js
+++ team2/index.js
@@ -1,3 +1,12 @@
 const sum = (a,b) => {	// summary for team projekt
     return a + b;
     };
+
+
+const mul = (a,b) => {  // multiply for team projekt
+    return a * b;
+    };
+
+const div = (a,b) => {  // divison for team projekt madebyKZ
+    return a/b;
+    };
