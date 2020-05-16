6ff55b59481836d07dc815ad5bb673c3eb0c6da3
javierfurus <javierfurus@gmail.com>
2020-05-16 10:02:01 +0200

    added div function

---
+++ team1/index.js
@@ -0,0 +1,9 @@
+// This lovely function will, as a well functioning democracy, divide things in accordance with it's master's plan. Divide a with b.
+const div = (a, b) => {
+  return a / b;
+}
+;
+module.exports = {
+  div: div
+}
+;
