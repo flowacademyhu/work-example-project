2c3b62aaca4e4fc608ef4390e83cdc2b84e092fb
Junászka Klaudia <klausz8801@gmail.com>
2020-05-16 10:37:38 +0200

    fix

--- team2/index.js
+++ team2/index.js
@@ -1,17 +1,11 @@
-const sum = (a,b) => {	// summary for team projekt
+const sum = (a, b) => {	// summary for team projekt
     return a + b;
-    };
-
-<<<<<<< HEAD
-let num = 10;
+};
 
 const mul = (a, b) => {
-    num *= 10;
-    console.log('INSIDE', num);
     return a * b;
 };
-=======
 module.exports = {
-	summary : sum
+    summary: sum,
+    mul: mul
 }
->>>>>>> ad93b7f4b887d5c30045c8176c4893f47007774c
