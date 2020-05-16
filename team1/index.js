afcdfbdedba48b8677e8b3d0f3022f1125707a5b
levee98 <leventet@t-email.hu>
2020-05-16 10:13:54 +0200

    add any rows

--- team1/index.js
+++ team1/index.js
@@ -1,7 +1,9 @@
 // This lovely function will, as a well functioning democracy, divide things in accordance with it's master's plan. Divide a with b.
+
 const div = (a, b) => {
   return a / b;
 };
+
 // mul
 
 const mul = (a, b) => {
@@ -13,6 +15,7 @@ const mul = (a, b) => {
 const sub = (a, b) => {
   return a - b;
 };
+
 // sum function
 const sum = (a, b) => {
   return a + b;
