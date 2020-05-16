9a13dc98a91b4995be14d1ce3ec6de7513d93ace
lucimc <lucimc@gmail.com>
2020-05-16 11:04:59 +0200

    add sub to team2

--- team2/index.js
+++ team2/index.js
@@ -1,28 +1,17 @@
-const sum = (a, b) => {	// summary for team projekt
-    return a + b;
+const sum = (a, b) => { // summary for team projekt
+  return a + b;
 };
 
 const mul = (a, b) => {
-    return a * b;
+  return a * b;
 };
-module.exports = {
-<<<<<<< HEAD
-	summary : sum
-            sub : sub
-}
-<<<<<<< HEAD
-
 
 const sub = (a, b) => { // sub  for team
- return a- b;
+  return a - b;
 };
 
-
-=======
->>>>>>> ad93b7f4b887d5c30045c8176c4893f47007774c
->>>>>>> 49cec2cdea6b324b1c89cdcb287fc460bc803ffe
-=======
-    summary: sum,
-    mul: mul
-}
->>>>>>> b1ce4984718c7613505ded12d6222e38812f39d6
+module.exports = {
+  summary: sum,
+  sub: sub,
+  mul: mul
+};
