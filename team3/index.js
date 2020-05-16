3a72636dfff0fab490dce42f144d234501fdf333
Dani <farkika39@gmail.com>
2020-05-16 10:01:52 +0200

    adding index.js

---
+++ team3/index.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+  console.log(num);
+  return a + b;
+};
+const sub = (a, b) => {
+  console.log(num);
+  return a - b;
+};
+const mul = (a, b) => {
+  console.log(num);
+  return a * b;
+};
+const div = (a, b) => {
+  console.log(num);
+  return a / b;
+};
+module.exports = {
+  sum,
+  sub,
+  mul,
+  div
+};
