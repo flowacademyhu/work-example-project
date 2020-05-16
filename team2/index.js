b245f59aac90e3b79a48bbfe809d3faa98994fca
Junászka Klaudia <klausz8801@gmail.com>
2020-05-16 10:10:07 +0200

    mul

--- team2/index.js
+++ team2/index.js
@@ -1,3 +1,11 @@
 const sum = (a,b) => {	// summary for team projekt
     return a + b;
     };
+
+let num = 10;
+
+const mul = (a, b) => {
+    num *= 10;
+    console.log('INSIDE', num);
+    return a * b;
+};
